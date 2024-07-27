f7cfa1926890375e5a145106de5e2747608d2fb4
mdnahidhosenn <mdnahid4652@gmail.com>
2024-07-27 18:14:32 +0600

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,12 @@
+<div class="link-section">
+          <a href="#" class="link-item">
+           <i class="uil uil-estate">Home
+         </a>
+         <a href="#" class="link-item">
+           <i class="uil uil-video">Shorts
+         </a>
+         <a href="#" class="link-item">
+           <i class="uil uil-tv-retro">Subscriptions
+         </a>
+         </div>
+         <div class="section-separator"></div>
